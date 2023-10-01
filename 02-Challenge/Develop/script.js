a87fb0c4ebf54f89abf847319a58477b4cf68e4c
bpavlis <benpavlis@gmail.com>
2023-10-01 18:57:00 -0500

    added prompts, and arrays, still working on generating it

--- 02-Challenge/Develop/script.js
+++ 02-Challenge/Develop/script.js
@@ -1,47 +1,65 @@
 // Assignment Code - DO NOT TOUCH BELOW
 var generateBtn = document.querySelector("#generate");
+//--------------------------------------
 
 
-
-var numberOfCharacters; //response will go into this var but this wasnt in global scope so moved up here 
-var includeNumbers; //only sending back true or false   
+// Global variables:
+var lengthOfChar; 
+var includeNumbers; 
 var includeSpecialCharacters;
+var includeUpper;
+var includeLower;
 
-var finalPassword = "";   //empty so can get changed
+var finalPassword;   
 
-
-
-function getARandomNumber(){  //just will get you a random # - only job
-  return 9;
+var charChoice = [];
+var charOptions = {
+  lowercase: ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
+  uppercase: ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"],
+  numeric: ["0","1","2","3","4","5","6","7","8","9"],
+  special: ["!","@","#","$","%","^","&","*",")","("],
 }
+//--------------------------------------
 
 
 function generatePassword(){
-  numberOfCharacters = prompt("How many characters?")
-  includeNumbers = confirm("Do you want numbers?")
 
-  var randomNumber = getARandomNumber() //this has one job so can be in the smaller scope
-  finalPassword = finalPassword + randomNumber;   //updated final password globally so all other functions can see/add 
+  // var randomNumber = getARandomNumber() //this has one job so can be in the smaller scope
+  // finalPassword = finalPassword + randomNumber;   //updated final password globally so all other functions can see/add 
 
 
-
+  for(var i = 0; i < lengthOfChar; i++){
+    var randomNum = Math.floor(Math.random() * charOptions);
+    password = password + charChoice[randomNum];    //turns password into what was chosen for charChoice
+  }
+  return password;
 }
 
 
 
+function promptAnswers(){
+  charChoice = [];    //to reset array so old data wasn't kept in there
+  lengthOfChar = parseInt(prompt("How many characters? Enter a value from 8 to 128"));
+  
+  includeNumbers = confirm("Do you want numbers?");   //returns true or false
+  includeSpecialCharacters = confirm("Do you want special characters?");
+  includeLower = confirm("Do you want special lowercase letters?");
+  includeUpper = confirm("Do you want special upercase letters?");
 
-
-
-
-// function generatePassword(){
-
-//   var name = prompt("What is your name?") //prompt - answer stored inside variable - might not leave this here? - this will get interpreted as a string - will learn how to change to #
-//   var confirm = confirm("This is a confirm button example") //confirm - true or false put inside this variable
-
-
-
-//   return ""
-// }
+  if (includeNumbers===true){   //own function?
+    charChoice = charChoice.concat(charOptions.numeric);    //concats numeric into empty array
+  }   //if not true - wont return anything
+  if (includeSpecialCharacters===true){   //own function?
+    charChoice = charChoice.concat(charOptions.special);    
+  }
+  if (includeLower===true){   //own function?
+    charChoice = charChoice.concat(charOptions.lowercase);    
+  }
+  if (includeUpper===true){   //own function?
+    charChoice = charChoice.concat(charOptions.uppercase);    
+  }
+  return true;
+}
 
 
 
@@ -53,8 +71,8 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
+//--------------------------------------
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
